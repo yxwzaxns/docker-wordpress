7d66732702809c319924f0f97cc7da3a31a1cf37
aong <yxwzaxns@gmail.com>
2016-03-02 18:13:31 +0800

    alter valume

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,7 @@ RUN mv /wordpress /var/www/
 RUN chown -R www-data:www-data /var/www/
 RUN chmod 755 /start.sh
 ENV WP_SOURCE /var/www
-ENV WP_CONTENT /var/wp
+ENV WP_CONTENT /var/www/wp_valume
 RUN mkdir -p "$WP_CONTENT" && chown -R user:user "$WP_CONTENT"
 VOLUME $WP_CONTENT
 EXPOSE 80
--- scripts/start.sh
+++ scripts/start.sh
@@ -2,17 +2,17 @@
 set -e
 
 # if [[ "$*" == npm*start* ]]; then
-	for dir in "$WP_SOURCE/wp-content"/*/; do
-		targetDir="$WP_CONTENT/$(basename "$dir")"
-		mkdir -p "$targetDir"
-		if [ -z "$(ls -A "$targetDir")" ]; then
-			tar -c --one-file-system -C "$dir" . | tar xC "$targetDir"
-		fi
-	done
+	# for dir in "$WP_SOURCE/wp-content"/*/; do
+	# 	targetDir="$WP_CONTENT/$(basename "$dir")"
+	# 	mkdir -p "$targetDir"
+	# 	if [ -z "$(ls -A "$targetDir")" ]; then
+	# 		tar -c --one-file-system -C "$dir" . | tar xC "$targetDir"
+	# 	fi
+	# done
 
-	# if [ ! -e "$WP_CONTENT/wp-config.php" ]; then
+	if [ ! -e "$WP_CONTENT/wp-config.php" ]; then
     cp "$WP_SOURCE/wp-config-sample.php" "$WP_CONTENT/wp-config.php"
-	# fi
+	fi
 
 	ln -sf "$WP_CONTENT/wp-config.php" "$WP_SOURCE/wp-config.php"
   echo "define( 'WP_CONTENT_DIR', '$WP_CONTENT' );" >> "$WP_CONTENT/wp-config.php"

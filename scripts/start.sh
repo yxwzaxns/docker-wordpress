fc556b27563f9084336f0b9f3cf55d959363ec56
aong <yxwzaxns@gmail.com>
2016-03-03 14:15:06 +0800

    a1

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,7 +13,7 @@ set -e
 	if [ ! -e "$VOLUME/wp-config.php" ]; then
     cp "$WP_SOURCE/wp-config-sample.php" "$VOLUME/wp-config.php"
     mv /var/www/wp-content /var/www/volume
-    sed -i '20a define( "WP_CONTENT_DIR", dirname(__FILE__)."/volume/wp-content" );' "$VOLUME/wp-config.php"
+    sed -i '20a define( "WP_CONTENT_DIR", dirname(__FILE__)."/volume/wp-content" );' /var/www/volume/wp-config.php
   fi
 
   ln -sf "$VOLUME/wp-config.php" "$WP_SOURCE/wp-config.php"
